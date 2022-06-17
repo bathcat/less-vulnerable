c933d08940388db39105d319c68c7b5fa1c87f81
bathcat <eric.mcmullen@gmail.com>
2022-06-17 07:41:25 -0500

    Refactoring style injection.

--- demos/acme/vanillajs-client/src/tools.mjs
+++ demos/acme/vanillajs-client/src/tools.mjs
@@ -1,7 +1,16 @@
+export const styles = `
+<style>
+  @import 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css';
+  @import 'https://bulma.io/vendor/fontawesome-free-5.15.2-web/css/all.min.css';
+</style>
+`;
+
 export async function fetchTemplate(current) {
-    const templatePath = current.replace('component.mjs', 'component.html');
-    const response = await fetch(templatePath);
-    const template = window.document.createElement('template');
-    template.innerHTML = await response.text();
-    return template;
-  }
+  const templatePath = current.replace('component.mjs', 'component.html');
+  const response = await fetch(templatePath);
+
+  const template = window.document.createElement('template');
+  template.innerHTML = styles + (await response.text());
+
+  return template;
+}
